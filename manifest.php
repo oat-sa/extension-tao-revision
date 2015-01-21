33a1553eff627d72e360bad22f1146d0b81da3ec
Joel Bout <joel.bout@taotesting.com>
2015-01-21 15:08:56 +0100

    set version to 1.0

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label' => 'Data Revision Control',
 	'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '0.1',
+    'version' => '1.0',
 	'author' => 'Open Assessment Technologies SA',
 	'requires' => array(
 	   'tao' => '>=2.7.4',
