54c3da768cda9f7462b8a38e06fed36939887dae
antoinerobin <antoine.robin@vesperiagroup.com>
2015-02-27 14:11:24 +0100

    see if it delete files

--- test/helper/DeleteHelperTest.php
+++ test/helper/DeleteHelperTest.php
@@ -14,8 +14,13 @@ class DeleteHelperTest extends \PHPUnit_Framework_TestCase {
     public function testDeepDelete(){
         //create resources
         $repository = \tao_models_classes_FileSourceService::singleton()->addLocalSource("Label Test", \tao_helpers_File::createTempDir());
-        $file = $repository->createFile("myTestfile.txt");
+        /** @var \core_kernel_versioning_File $file */
+        $file = $repository->createFile("test.xml", "sample");
 
+        mkdir($repository->getPath().'sample');
+        copy(__DIR__.'/sample/test.xml', $repository->getPath().'sample/test.xml');
+        $dirname = $file->getFileInfo()->getPath();
+        $this->assertFileExists($dirname.'/test.xml');
         //delete resource
         DeleteHelper::deepDelete($file);
         DeleteHelper::deepDelete($repository);
@@ -24,15 +29,17 @@ class DeleteHelperTest extends \PHPUnit_Framework_TestCase {
         //try to get the resource
         $resourceTest = new \core_kernel_classes_Resource($repository->getUri());
         $fileTest = new \core_kernel_classes_Resource($file->getUri());
-
+\common_Logger::w($dirname);
+        $this->assertFileNotExists($dirname.'/test.xml');
         $this->assertCount(0, $resourceTest->getRdfTriples());
         $this->assertCount(0, $fileTest->getRdfTriples());
+
     }
 
     public function testDeepDeleteTriples(){
         //create resources
         $repository = \tao_models_classes_FileSourceService::singleton()->addLocalSource("Label Test", \tao_helpers_File::createTempDir());
-        $file = $repository->createFile("myTestfile.txt");
+        $file = $repository->createFile("test.xml", "sample");
 
         //delete resource
         DeleteHelper::deepDeleteTriples($file->getRdfTriples());
@@ -42,7 +49,6 @@ class DeleteHelperTest extends \PHPUnit_Framework_TestCase {
         //try to get the resource
         $resourceTest = new \core_kernel_classes_Resource($repository->getUri());
         $fileTest = new \core_kernel_classes_Resource($file->getUri());
-
         $this->assertCount(0, $resourceTest->getRdfTriples());
         $this->assertCount(0, $fileTest->getRdfTriples());
     }
