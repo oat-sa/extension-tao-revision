93422e61a68915150b355891663f7a5557491ee3
Ionut Padurariu <ionut@taotesting.com>
2017-10-06 16:05:33 +0200

    increase version

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
     'label' => 'Data Revision Control',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '3.0.0',
+    'version' => '3.0.1',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
 	   'tao' => '>=9.0.0',
