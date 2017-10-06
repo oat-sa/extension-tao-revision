9294066fc5dc343a8e5b87813071053a0d0b9182
bugalot <jerome@taotesting.com>
2017-10-06 15:34:18 +0200

    Bump to version 4.0.0

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
     'label' => 'Data Revision Control',
     'description' => '',
     'license' => 'GPL-2.0',
-    'version' => '3.0.0',
+    'version' => '4.0.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
        'generis' => '>=5.0.0',
