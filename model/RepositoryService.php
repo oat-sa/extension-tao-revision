53f08003f5ad529b0ebb76be5f0d169febb8fd03
siwane <camille@taotesting.com>
2019-04-18 14:01:28 +0200

    Stricly check if $user is null

--- model/RepositoryService.php
+++ model/RepositoryService.php
@@ -94,7 +94,7 @@ class RepositoryService extends ConfigurableService implements Repository
     {
         if ($userId === null) {
             $user = \common_session_SessionManager::getSession()->getUser();
-            $userId = is_null($user) ? '' : $user->getIdentifier();
+            $userId = ($user === null) ? '' : $user->getIdentifier();
         }
         $version = is_null($version) ? $this->getNextVersion($resourceId) : $version;
         $created = time();
