7fb139d7086e29d55b839d5bfbd7a681eeb8f598
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-02-18 23:20:29 +0300

    Remove very old code and set latest supported version to 2.0.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -23,80 +23,43 @@
 namespace oat\taoRevision\scripts\update;
 
 use common_Exception;
-use common_exception_Error;
-use common_ext_ExtensionException;
+use common_exception_NotImplemented as NotImplementedException;
+use common_ext_ExtensionUpdater;
 use oat\oatbox\filesystem\FileSystemService;
-use oat\tao\model\accessControl\func\AccessRule;
-use oat\tao\model\accessControl\func\AclProxy;
-use oat\taoRevision\model\Repository;
-use oat\taoRevision\model\rds\Storage;
+use oat\taoRevision\model\RepositoryInterface;
 use oat\taoRevision\model\RepositoryService;
 
 /**
  *
  * @author Joel Bout <joel@taotesting.com>
  */
-class Updater extends \common_ext_ExtensionUpdater
+class Updater extends common_ext_ExtensionUpdater
 {
     /**
-     *
      * @param string $initialVersion
      *
      * @return string $versionUpdatedTo
      * @throws common_Exception
-     * @throws common_exception_Error
-     * @throws common_ext_ExtensionException
      */
     public function update($initialVersion)
     {
-        $currentVersion = $initialVersion;
-
-        //migrate from 1.0 to 1.0.1
-        if ($currentVersion == '1.0') {
-            AclProxy::applyRule(new AccessRule(
-                'grant',
-                'http://www.tao.lu/Ontologies/TAOItem.rdf#ItemAuthor',
-                ['controller' => 'oat\\taoRevision\\controller\\History']
-            ));
-            AclProxy::applyRule(new AccessRule(
-                'grant',
-                'http://www.tao.lu/Ontologies/TAOItem.rdf#TestAuthor',
-                ['controller' => 'oat\\taoRevision\\controller\\History']
-            ));
-            $currentVersion = '1.0.1';
-        }
-
-        $this->setVersion($currentVersion);
-
-        $this->skip('1.0.1', '1.0.4');
-
-        if ($this->isVersion('1.0.4')) {
-            $impl = $this->getServiceManager()->get(Repository::SERVICE_ID);
-            if ($impl instanceof \oat\taoRevision\model\rds\Repository) {
-                $storage = new Storage($impl->getOptions());
-                $this->getServiceManager()->register('taoRevision/storage', $storage);
-
-                $service = new RepositoryService([
-                    RepositoryService::OPTION_STORAGE => 'taoRevision/storage',
-                ]);
-                $this->getServiceManager()->register(Repository::SERVICE_ID, $service);
-            }
-            $this->setVersion('2.0.0');
+        if ($this->isBetween('1.0.0', '2.0.0')) {
+            throw new NotImplementedException('Updates from versions prior to 2.0.0 are not longer supported');
         }
 
         $this->skip('2.0.0', '2.1.2');
 
         if ($this->isVersion('2.1.2')) {
-            $fsm = $this->getServiceManager()->get(FileSystemService::SERVICE_ID);
-            $fsm->createFileSystem('revisions', 'tao/revisions');
-            $this->getServiceManager()->register(FileSystemService::SERVICE_ID, $fsm);
+            $fss = $this->getServiceManager()->get(FileSystemService::SERVICE_ID);
+            $fss->createFileSystem(RepositoryService::FILES_SYSTEM_NAME, 'tao/revisions');
+            $this->getServiceManager()->register(FileSystemService::SERVICE_ID, $fss);
 
-            $repositoryService = $this->getServiceManager()->get(Repository::SERVICE_ID);
-            $repositoryService->setOption(RepositoryService::OPTION_FS, 'revisions');
-            $this->getServiceManager()->register(Repository::SERVICE_ID, $repositoryService);
+            $repositoryService = $this->getServiceManager()->get(RepositoryInterface::SERVICE_ID);
+            $repositoryService->setOption(RepositoryService::OPTION_FS, RepositoryService::FILES_SYSTEM_NAME);
+            $this->getServiceManager()->register(RepositoryInterface::SERVICE_ID, $repositoryService);
             $this->setVersion('2.2.0');
         }
 
-        $this->skip('2.2.0', '7.1.0');
+        $this->skip('2.2.0', '8.0.0');
     }
 }
